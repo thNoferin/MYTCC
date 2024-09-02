c3ba76f0c257aaa269958d878d74bd3a84e576de
thNoferin <nataestevesdesouza@gmail.com>
2024-09-02 00:08:09 -0300

    Oi

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
-const port = 3001;  // Alterar para uma porta diferente se 3000 estiver em uso
+
+// Use a porta fornecida pela variável de ambiente PORT
+const port = process.env.PORT || 3001; 
 
 app.use(express.json());
 app.use(cors());
 
 app.post('/send-message', async (req, res) => {
     try {
-        // Envia a mensagem para o Node-RED
-        const response = await axios.post('http://127.0.0.1:1880/endpoint', {
+        // Substitua pelo URL público do Node-RED
+        const response = await axios.post('http://PUBLIC_IP_OR_URL:1880/endpoint', {
             message: 'Oi'  // Mensagem que você quer enviar
         });
         res.status(200).send('Mensagem enviada com sucesso!');
@@ -21,6 +23,7 @@ app.post('/send-message', async (req, res) => {
     }
 });
 
+// Inicia o servidor na porta especificada
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando na porta ${port}`);
 });
